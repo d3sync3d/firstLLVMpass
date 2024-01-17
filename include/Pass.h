5038e2096e5d2b1aa6bf7d77f37916c9f8c29439
d3sync3d <d35sync3d@protonmail.com>
2024-01-17 18:46:17 +0530

    code incoming

---
+++ include/Pass.h
@@ -0,0 +1,6 @@
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/CFG.h"
---
+++ src/FirstLLVMconsumer.c
@@ -0,0 +1,17 @@
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+#include "Pass.h"
+
+using namespace llvm;
+
+int main(int argc, char** argv){
+    return 0;
+}
